773c9f7fb4b9d77f14c5d9fee89caa8fba0621f0
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-04-12 00:06:21 -0400

    Sample quill query

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,20 +4,34 @@ import com.twitter.util.Await
 import io.finch._
 import io.finch.circe._
 import io.circe.generic.auto._
+import io.getquill._
 
 /**
   * Created by panagiotis on 11/04/17.
   */
 
+case class Person(id: Int, name: String, age: Int)
+case class Pet(ownerId: Int, name: String)
+
 object Main extends App {
-  case class Person(name: String, age: Int)
+  val ctx = new SqlMirrorContext[MirrorSqlDialect, SnakeCase]
+  import ctx._
+
+  val q = quote {
+    for {
+      person <- query[Person].filter(_.name == "Panagiotis")
+      pet <- query[Pet].filter(_.ownerId == person.id)
+    } yield (person.name, pet.name)
+  }
+
+  println(ctx.run(q))
 
   val hi: Endpoint[Person] = get("abc") {
-    Ok(Person("Panagiotis", 20))
+    Ok(Person(1, "Panagiotis", 20))
   }
 
   val bye: Endpoint[Person] = get("xyz") {
-    Ok(Person("Roubatsis", 1000))
+    Ok(Person(2, "Roubatsis", 1000))
   }
 
   val combined: Service[Request, Response] = (hi :+: bye).toService
